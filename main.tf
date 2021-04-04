66a06257f59dea1f47c2a525aaf7641179f50bfa
Alexander Mancevice <alexander.mancevice@hey.com>
2021-04-04 11:22:51 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-data aws_iam_policy_document assume_role {
+data "aws_iam_policy_document" "assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
 
@@ -9,24 +9,24 @@ data aws_iam_policy_document assume_role {
   }
 }
 
-resource aws_iam_role role {
+resource "aws_iam_role" "role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
   name               = var.name
 }
 
-resource aws_iam_role_policy_attachment basic {
+resource "aws_iam_role_policy_attachment" "basic" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
   role       = aws_iam_role.role.name
 }
 
-resource aws_iam_role_policy_attachment additional {
+resource "aws_iam_role_policy_attachment" "additional" {
   count      = length(var.attachments)
   policy_arn = element(var.attachments, count.index)
   role       = aws_iam_role.role.name
 }
 
-resource aws_iam_role_policy inline {
-  count      = length(var.inline_policies)
-  policy     = element(var.inline_policies, count.index)
-  role       = aws_iam_role.role.id
+resource "aws_iam_role_policy" "inline" {
+  count  = length(var.inline_policies)
+  policy = element(var.inline_policies, count.index)
+  role   = aws_iam_role.role.id
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
-variable name {
+variable "name" {
   description = "IAM role name."
 }
 
-variable inline_policies {
+variable "inline_policies" {
   description = "Optional additional inline IAM policy documents."
   type        = list(string)
   default     = []
 }
 
-variable attachments {
+variable "attachments" {
   description = "Optional additional attached IAM policy ARNs."
   type        = list(string)
   default     = []
