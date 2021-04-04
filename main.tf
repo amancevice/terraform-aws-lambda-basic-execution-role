1affa539ed70ecb746c1d3002cfd06cebf605f9e
Alexander Mancevice <alexander.mancevice@hey.com>
2021-04-04 11:28:56 -0400

    Add terraform version constraints

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "~> 0.13"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 data "aws_iam_policy_document" "assume_role" {
   statement {
     actions = ["sts:AssumeRole"]
